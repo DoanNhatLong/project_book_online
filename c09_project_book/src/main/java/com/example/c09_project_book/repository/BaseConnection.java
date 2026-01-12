694a15188853f40845770431a9561035733b9c5a
Long <longs3515@gmail.com>
2026-01-12 15:31:50 +0700

    first

---
+++ c09_project_book/src/main/java/com/example/c09_project_book/repository/BaseConnection.java
@@ -0,0 +1,21 @@
+package com.example.c09_project_book.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseConnection {
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/project";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ad1412";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException("Không thể kết nối DB", e);
+        }
+    }
+}
